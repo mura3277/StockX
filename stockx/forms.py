d99e5a8d1422ecaa41c557a786289d365447f9cd
Muramasa <haydenkilloh@gmail.com>
2021-06-08 20:46:58 +0100

    cart working and item pages implemented

--- stockx/forms.py
+++ stockx/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed
-from wtforms import StringField, SubmitField, FileField
+from wtforms import StringField, SubmitField, FileField, IntegerField
 from wtforms.validators import DataRequired, Length
+from wtforms.widgets.html5 import NumberInput
+
 
 class ShoeForm(FlaskForm):
     kw = {"title": "Shoe Form", "id": "item-form"}
@@ -15,3 +17,40 @@ class SearchForm(FlaskForm):
     kw = {"id": "search-form", "ignore_labels": True}
     input = StringField("Search", validators=[DataRequired(), Length(min=2, max=64)])
     submit = SubmitField("Submit")
+
+"""
+A AddCartForm class, inheriting from FlaskForm
+For rendering a cart to cart form on every item page
+"""
+class AddCartForm(FlaskForm):
+    kw = {"id": "cart-form"}
+    quantity = IntegerField(widget=NumberInput(min=1), default=1)
+    submit = SubmitField("Add To Cart")
+
+"""
+A IncCartForm class, inheriting from FlaskForm
+For rendering an increment item count button on every item in the user's cart
+"""
+class IncCartForm(FlaskForm):
+    submit = SubmitField("+")
+
+"""
+A DecCartForm class, inheriting from FlaskForm
+For rendering a decrement item count button on every item in the user's cart
+"""
+class DecCartForm(FlaskForm):
+    submit = SubmitField("-")
+
+"""
+A RemCartForm class, inheriting from FlaskForm
+For rendering a remove item button on every item in the user's cart
+"""
+class RemCartForm(FlaskForm):
+    submit = SubmitField("Remove")
+
+"""
+A EmptyCartForm class, inheriting from FlaskForm
+Quality of life button to clear the entire cart for the user
+"""
+class EmptyCartForm(FlaskForm):
+    submit = SubmitField("Clear Cart")
--- stockx/routes.py
+++ stockx/routes.py
@@ -2,8 +2,8 @@ from flask import render_template, request
 from werkzeug.exceptions import abort
 
 from stockx import app, db
-from stockx.forms import ShoeForm, SearchForm
-from stockx.helper import to_img64
+from stockx.forms import ShoeForm, SearchForm, AddCartForm, EmptyCartForm, RemCartForm, DecCartForm, IncCartForm
+from stockx.helper import to_img64, update_cart, get_cart, purge_cart
 from stockx.models import Shoe
 
 def render(template, **kwargs):
@@ -19,8 +19,37 @@ def index():
     return render("index.jinja")
 
 @app.route("/items", methods=["GET", "POST"])
-def inventory():
+@app.route("/items/<brand>", methods=["GET", "POST"])
+def inventory(brand=None):
+    # If brand exists, show brand specific entries
+    if brand:
+        return render("items.jinja", items=Shoe.query.filter_by(brand=brand).all())
     return render("items.jinja", items=Shoe.query.all())
+
+@app.route("/item/<int:item_id>", methods=["GET", "POST"])
+def item(item_id):
+    cart_form = AddCartForm()
+    item = Shoe.query.get_or_404(item_id)
+    # If we are posting to this route and the cart form is valid, push item into the session cart
+    if request.method == "POST" and cart_form.validate_on_submit():
+        update_cart(item, cart_form.quantity.data)
+    return render("item.jinja", item=item, cart_form=cart_form)
+
+@app.route("/cart", methods=["GET", "POST"])
+@app.route("/cart/<item_id>/<count>", methods=["GET", "POST"])
+def cart(item_id=None, count=None):
+    #If item_id and count exists, we are updating a specific cart entry
+    if item_id and count:
+        update_cart(Shoe.query.get_or_404(item_id), count, True)
+    #Build the subtotal for rendering with Jinja
+    subtotal = 0
+    for i in get_cart():
+        subtotal += (i["item"].price * i["quantity"])
+    #Check if the user wants to clear the cart
+    empty_cart_form = EmptyCartForm()
+    if not item_id and request.method == "POST" and empty_cart_form.validate_on_submit():
+        purge_cart()
+    return render("cart.jinja", items=get_cart(), empty_cart_form=empty_cart_form, inc_cart_form=IncCartForm(), dec_cart_form=DecCartForm(), rem_cart_form=RemCartForm(), subtotal=subtotal)
 
 @app.route("/about")
 def about():
@@ -29,10 +58,6 @@ def about():
 @app.route("/contact")
 def contact():
     return render("contact.jinja")
-
-@app.route("/cart")
-def cart():
-    return render("cart.jinja")
 
 """
 Development route. Used for making inserting data easy
