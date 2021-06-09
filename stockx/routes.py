4ccfdf460bbde2fb0d0737402bcf608b5a9db1ee
Muramasa <haydenkilloh@gmail.com>
2021-06-09 22:27:04 +0100

    new items working, and now links to actual product pages

--- stockx/routes.py
+++ stockx/routes.py
@@ -19,7 +19,8 @@ def render(template, **kwargs):
 @app.route("/")
 def index():
     popular = [Shoe.query.get_or_404(2), Shoe.query.get_or_404(3), Shoe.query.get_or_404(4), Shoe.query.get_or_404(5)]
-    return render("index.jinja", popular=popular)
+    new = [Shoe.query.get_or_404(2), Shoe.query.get_or_404(3), Shoe.query.get_or_404(4), Shoe.query.get_or_404(5)]
+    return render("index.jinja", popular=popular, new=new)
 
 @app.route("/items", methods=["GET", "POST"])
 @app.route("/items/<brand>", methods=["GET", "POST"])
